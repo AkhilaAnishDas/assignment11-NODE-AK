658c8a9ff30c1e6567a0a402d6ce50943dc4b7e5
AkhilaAnishDas <akhilaanish2287@gmail.com>
2026-09-17 14:34:15 +0530

    first commit

---
+++ schema/teacherSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const teacherSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+
+    subject: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+module.exports = teacherSchema;
